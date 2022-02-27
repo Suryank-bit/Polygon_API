2c11247e47e913ab073b82f9e21044abb428bdb7
Suryank-bit <68577418+Suryank-bit@users.noreply.github.com>
2022-02-27 22:16:56 +0530

    Update project.py

--- project.py
+++ project.py
@@ -7,6 +7,21 @@ def ts_to_datetime(ts) -> str:
 
 
 def data_main(stockTicker,multipliar,indexFormat,_from,_to):
+    """
+    stockTicker: the ticker symbol for the company which's prices you want to plot
+
+    indexFormat: what kind of time variation you want eg:- date to date or hour to hour
+
+    multiplier: add on to time variation eg:- if indexFormat is 'hour' and multiplier is 1
+    means the next price it will receive will be 1 hour in future from the previous time.
+
+    _from: start date of data you want to receive(str format)
+
+    _to: till which date you want the data to be received(str)
+    ----------------------------------------------------------------------------------
+    return: resp a json file having data needed
+    
+    """
     API_KEY="Your API Token"
     with RESTClient(API_KEY) as client:
 
